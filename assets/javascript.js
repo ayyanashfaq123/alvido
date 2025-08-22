97b29155092e6a052956252cc5c19da9888aa98c
ayyanashfaq123 <ayyanrehmani022@gmail.com>
2025-08-23 03:14:36 +0500

    complete

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,10 +1,12 @@
-const sidebar = document.querySelector(".sidebar")
-function showNav() {
-    if(sidebar.style.display === 'none'){
-        sidebar.style.display = 'block'
-        sidebar.style.transition = "all 5s";
-    }
-    else{
-        sidebar.style.display = 'none'
-    }
-}
+
+const sidebar = document.getElementById('sidebar');
+const menuBtn = document.getElementById('menuBtn');
+const closeBtn = document.getElementById('closeBtn');
+
+menuBtn.addEventListener('click', () => {
+  sidebar.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('active');
+});
